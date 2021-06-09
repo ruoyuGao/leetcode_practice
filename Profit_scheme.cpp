0b475f5aee39cd5d6bec29ff99f22b3821823307
Aron751 <gaoruoyu751@gmail.com>
2021-06-09 19:16:35 +0800

    2021/06/09-1
    动态规划和位于运算（本质上和回溯一样，都是dfs）

--- Profit_scheme.cpp
+++ Profit_scheme.cpp
@@ -27,4 +27,23 @@ public:
         }
         return count;
     }
+    //动态规划，本来应该是三维的，但是因为i，也就是dp当前可选择的工作，已选择的小组员工人数，以及目前状态的工作获利下限时候的方案数量。同时因为前i个工作只和i-1系列的内容相关，因此可以降维
+    //https://leetcode-cn.com/problems/profitable-schemes/solution/ying-li-ji-hua-by-leetcode-solution-3t8o/
+    //
+    int profitableSchemes2(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        vector<vector<int>> dp(n + 1, vector<int>(minProfit + 1));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        int len = group.size(), MOD = (int)1e9 + 7;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = n; j >= members; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    dp[j][k] = (dp[j][k] + dp[j - members][max(0, k - earn)]) % MOD;
+                }
+            }
+        }
+        return dp[n][minProfit];
+    }
 };
